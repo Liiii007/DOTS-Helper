618d8218eb1c5528a9417a652a89ada23f71e725
Liiii <liiii007@outlook.com>
2023-03-21 22:43:46 +0800

    Init and add Native2DArray

---
+++ NativeContainer/Tests/Native2DArrayTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Native2DArrayTest
+{
+    [Test]
+    public void Native2DArrayTestSimplePasses()
+    {
+        List<List<int>> ints= new List<List<int>>();
+        ints.Add(new List<int>(new int[] { 1, 2, 3 }));
+        ints.Add(new List<int>(new int[] { 4, 5 }));
+        ints.Add(new List<int>(new int[] { 6, 7, 8, 9 }));
+
+        using(Native2DArray<int> native2DArray = new(ints, Unity.Collections.Allocator.TempJob))
+        {
+            native2DArray.Set(0, 2, 5);
+
+            Assert.AreEqual(1, native2DArray.Get(0, 0));
+            Assert.AreEqual(5, native2DArray.Get(0, 2));
+            Assert.AreEqual(5, native2DArray.Get(1, 1));
+            Assert.AreEqual(9, native2DArray.Get(2, 3));
+            //native2DArray.Get(9, 9);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DOTS-Helper
-A set of helper for Unity DOTS and ECS Architecture
+A set of helper for Unity DOTS and ECS Architecture. 
+Tested on Unity 2022.2.11f1.
+
+## Contain
+- Native2DArray(unsafe) : Use inside ComponentData struct, work with System and Burst compiler. Mild Performance Lost.
+
+## Warning 
+- Not Fully Test.
+- Not Thread-safe and Atomic.
+- Unstable, maybe work?
